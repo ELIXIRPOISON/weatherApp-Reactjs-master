ba7a5ba15f816be9d05d915d7d8f8cbe4b8cffeb
PIYUSH RAJ <therajpiyush@gmail.com>
2024-01-30 00:00:42 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9d366ec50b662c3b2c4d600f49289dbe",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
